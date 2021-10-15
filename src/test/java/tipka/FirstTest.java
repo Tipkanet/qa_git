9961cb39667dede294881ffb218a55391be3c74f
tipka <tipkanet@gmail.com>
2021-10-15 13:29:41 +0300

    add first test

---
+++ src/test/java/tipka/FirstTest.java
@@ -0,0 +1,13 @@
+package tipka;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void assertThatTrue () {
+        Assertions.assertTrue(true);
+    }
+
+}
